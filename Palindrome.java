b101515ee598bb7a58a1cde50582f9c27b4024eb
Akshit2238 <akshit.2025.2029@gmail.com>
2026-04-06 14:17:20 +0530

    Initial commit with 5 basic Java codes

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package lib.College;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rev = 0; // to find reverse
+        int z; // used because the original value will become 0
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        z = num;
+        while (num > 0) {
+            rev = rev * 10 + num % 10;
+            num = num / 10;
+        }
+        if (rev == z) {
+            System.out.println("The number is a palindrome");
+        } else {
+            System.out.println("The number is not a palindrome");
+        }
+    }
+}
