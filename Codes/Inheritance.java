c48fb427b54ccb446e4062fb496cdbb2f8f4b535
Akshit2238 <akshit.2025.2029@gmail.com>
2026-04-07 11:59:51 +0530

    Add Inheritance.java

---
+++ Codes/Inheritance.java
@@ -0,0 +1,24 @@
+class Vehicle {
+    public void run() {
+        System.out.println("Vehicle can run");
+    }
+}
+
+class Car extends Vehicle {
+    public void fuel() {
+        System.out.println("Car uses fuel");
+    }
+}
+
+class ElectricCar extends Car {
+    public void battery() {
+        System.out.println("Electric car uses battery");
+    }
+
+    public static void main(String[] args) {
+        ElectricCar e = new ElectricCar();
+        e.fuel();
+        e.battery();
+        e.run();
+    }
+}
