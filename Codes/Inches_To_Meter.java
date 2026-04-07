361a965f576b8dba58bd082272886039380752a1
Akshit2238 <akshit.2025.2029@gmail.com>
2026-04-07 11:55:21 +0530

    Add Inches_To_Meter.java

---
+++ Codes/Inches_To_Meter.java
@@ -0,0 +1,13 @@
+package lib.College;
+
+import java.util.Scanner;
+
+public class Inches_To_Meter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number in inches :");
+        double a = sc.nextDouble();
+        double meter = a * 0.0254;
+        System.out.println("Inches into meter: " + meter);
+    }
+}
