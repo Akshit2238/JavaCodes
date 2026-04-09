c3b86de54b75e5f498f895f7af721984464a1d2c
Akshit2238 <akshit.2025.2029@gmail.com>
2026-04-09 11:18:40 +0530

    Add Factorial_Using_Functions.java

---
+++ Codes/Factorial_Using_Functions.java
@@ -0,0 +1,22 @@
+package lib.College;
+
+import java.util.Scanner;
+
+public class Factorial_Using_Functions {
+
+    public static int fact(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int num = sc.nextInt();
+        int result = fact(num);
+        System.out.println("Factorial of " + num + " is " + result);
+        sc.close();
+    }
+}
