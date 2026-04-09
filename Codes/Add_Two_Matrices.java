d6015214d0bf270980ef6abdbf732a3bd7dfad3c
Akshit2238 <akshit.2025.2029@gmail.com>
2026-04-09 11:18:29 +0530

    Add Add_Two_Matrices.java

---
+++ Codes/Add_Two_Matrices.java
@@ -0,0 +1,39 @@
+package lib.College;
+
+import java.util.Scanner;
+
+public class Add_Two_Matrices {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns:");
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int[][] a = new int[rows][cols];
+        int[][] b = new int[rows][cols];
+        int[][] c = new int[rows][cols];
+        System.out.println("Enter the elements of the first matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter the elements of the second matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        System.out.println("The sum of the two matrices is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
