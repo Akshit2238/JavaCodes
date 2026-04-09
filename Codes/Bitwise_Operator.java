58c1ce8c53ff6a8bf46bda1800f39b7f3d758bac
Akshit2238 <akshit.2025.2029@gmail.com>
2026-04-09 11:18:33 +0530

    Add Bitwise_Operator.java

---
+++ Codes/Bitwise_Operator.java
@@ -0,0 +1,18 @@
+package lib.College;
+
+import java.util.Scanner;
+
+public class Bitwise_Operator {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.err.print("Enter Number a:");
+            int a = sc.nextInt();
+            System.err.print("Enter Number b:");
+            int b = sc.nextInt();
+            System.err.println("a&b = " + (a & b));
+            System.err.println("a|b = " + (a | b));
+            System.err.println("a<<1 = " + (a << 1));
+            System.err.println("a>>2 = " + (a >> 2));
+        }
+    }
+}
