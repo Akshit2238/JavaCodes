9290cda8af56161b145b3bca049fc69438623d9b
Akshit2238 <akshit.2025.2029@gmail.com>
2026-04-10 13:53:10 +0530

    Add Living_Inheritance.java

---
+++ Codes/Living_Inheritance.java
@@ -0,0 +1,30 @@
+package Codes;
+class Living {
+    public void run() {
+        System.out.println("All Living being have brain ");
+    }
+}
+    class animal extends Living{
+        public void eat(){
+            System.out.println("Animal can run");
+        }
+    }
+    class bird extends animal{
+        public void fly(){
+            System.out.println("Bird can fly");
+        }
+    }
+    class parrot extends bird {
+        public void speak() {
+            System.out.println("Parrot can speak");
+        }
+
+        public static void main(String[] args) {
+            parrot pc = new parrot();
+            pc.run();
+            pc.fly();
+            pc.eat();
+            pc.speak();
+        }
+    }
+
