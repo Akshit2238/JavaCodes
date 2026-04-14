7d3fa784c9082a986a6a702e1dfe2757030f0404
Akshit2238 <akshit.2025.2029@gmail.com>
2026-04-14 18:56:53 +0530

    Add Singledim_Array.java

---
+++ Codes/Singledim_Array.java
@@ -0,0 +1,21 @@
+package Codes;
+import java.util.Scanner;
+
+public class Singledim_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter Elements:");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        sc.close();
+    }
+}
