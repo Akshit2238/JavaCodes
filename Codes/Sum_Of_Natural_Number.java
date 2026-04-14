9955a645b146a10c43f61bc12d9bbe0b82909c2e
Akshit2238 <akshit.2025.2029@gmail.com>
2026-04-14 18:56:53 +0530

    Add Sum_Of_Natural_Number.java

---
+++ Codes/Sum_Of_Natural_Number.java
@@ -0,0 +1,18 @@
+package Codes;
+import java.util.Scanner;
+
+public class Sum_Of_Natural_Number {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int number = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of first " + number + " natural numbers is: " + sum);
+        scanner.close();
+    }
+}
+
+
