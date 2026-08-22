8994aa3d727fa2fcfb9f6a773ef81e3d2dbbe53e
Akshit2238 <258777910+Akshit2238@users.noreply.github.com>
2026-08-22 19:40:46 +0530

    Add files via upload
    Boolean

---
+++ BooleanInJava.java
@@ -0,0 +1,11 @@
+
+public class BooleanInJava {
+
+	public static void main(String[] args) {
+		boolean b = true; //boolean is always true or false
+		
+		if(true) {
+			System.out.println("We are on Pluto!!!!");			
+		}
+	}
+}
